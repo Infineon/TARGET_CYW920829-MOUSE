98ecc8588bfe6bdd798c154a7990433f55b0e911
gitlab-runner <do-not-reply@cypress.com>
2024-08-12 18:25:34 -0500

    Upload TARGET_CYW920829-MOUSE [2665]

--- docs/html/group__group__bsp__pins.js
+++ docs/html/group__group__bsp__pins.js
@@ -1,6 +1,6 @@
 var group__group__bsp__pins =
 [
-    [ "LED Pins", "group__group__bsp__pins__led.html", null ],
-    [ "Button Pins", "group__group__bsp__pins__btn.html", null ],
-    [ "Communication Pins", "group__group__bsp__pins__comm.html", null ]
+    [ "LED Pins", "group__group__bsp__pins__led.html", "group__group__bsp__pins__led" ],
+    [ "Button Pins", "group__group__bsp__pins__btn.html", "group__group__bsp__pins__btn" ],
+    [ "Communication Pins", "group__group__bsp__pins__comm.html", "group__group__bsp__pins__comm" ]
 ];
